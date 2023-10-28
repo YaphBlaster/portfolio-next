1a71116fa33834c08dd25ff485525457685ac759
Yaphet Abraha <yaphet.abraha@gmail.com>
2023-10-28 14:21:35 -0500

    add shadUI

--- tailwind.configs.ts
+++ tailwind.configs.ts
@@ -1,12 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  // content: [
+  //   './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  //   './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  //   './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  // ].
+  // plugins: [],
+
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -58,12 +66,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -73,4 +81,5 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
+export default config;
