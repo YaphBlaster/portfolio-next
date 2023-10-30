89a39808f832d5b226e34910b32171221b7f7150
Yaphet Abraha <yaphet.abraha@gmail.com>
2023-10-30 09:18:58 -0500

    added prisma seed steps

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import Portfolio from "@/components/Portfolio";
 
-const getUserById = async (id: string) => {
-  return (await fetch(`/api/${id}`)).json();
+const getPageOwnerById = async (id: string) => {
+  return (await fetch(`/api/pageOwner/${id}`)).json();
 };
 
 type Props = {};
 
-const User = ({ params }: { params: { id: string } }) => {
+const PageOwnerPage = ({ params }: { params: { id: string } }) => {
   const { data } = useQuery({
     queryKey: ["user", params.id],
-    queryFn: () => getUserById(params.id),
+    queryFn: () => getPageOwnerById(params.id),
   });
   return (
     <div>
+      <Portfolio pageOwner={data} />
       <div>{params.id}</div>
       <div>{data?.name}</div>
     </div>
   );
 };
 
-export default User;
+export default PageOwnerPage;
