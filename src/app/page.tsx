3a1f36ac73ca740d5d29d76edbbfda85a45b0cd0
Yaphet Abraha <yaphet.abraha@gmail.com>
2023-10-31 12:47:15 -0500

    added sheet component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ThemeToggle from "@/components/ThemeToggle";
 import Portfolio from "@/components/Portfolio";
 import { dbUtils } from "@/lib/db";
+import AdminSheet from "@/components/AdminSheet";
 
 export default async function Home() {
   const data = await dbUtils.getPageOwner({});
@@ -10,6 +11,7 @@ export default async function Home() {
   return (
     <div>
       <ThemeToggle />
+      <AdminSheet />
       <div>Nav bar</div>
       {/* <div>Image?</div> */}
       <Portfolio />
---
+++ src/components/AdminSheet.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+type Props = {};
+
+const AdminSheet = (props: Props) => {
+  return (
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default AdminSheet;
